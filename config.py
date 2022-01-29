657b82efba06f514eeb7c5cf35e997f952377ddc
nelsonchoo456 <nelsonchoo456@gmail.com>
2022-01-30 01:04:28 +0800

    edited config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-ANGEL_BOT_TOKEN = os.getenv("ANGEL_BOT_TOKEN")
-PLAYERS_FILENAME = os.getenv("PLAYERS_FILENAME")
-CHAT_ID_JSON = os.getenv("CHAT_ID_JSON")
-ANGEL_ALIAS = os.getenv("ANGEL_ALIAS")
-MORTAL_ALIAS = os.getenv("MORTAL_ALIAS")
+ANGEL_BOT_TOKEN = os.environ["ANGEL_BOT_TOKEN"]
+PLAYERS_FILENAME = os.environ["PLAYERS_FILENAME"]
+CHAT_ID_JSON = os.environ["CHAT_ID_JSON"]
+ANGEL_ALIAS = os.environ["ANGEL_ALIAS"]
+MORTAL_ALIAS = os.environ["MORTAL_ALIAS"]
